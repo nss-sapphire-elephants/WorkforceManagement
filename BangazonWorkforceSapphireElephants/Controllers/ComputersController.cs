a5a523016cfde9ac0857ec813f19ad5462110ed4
unknown <huntermettsdev@gmail.com>
2019-04-10 13:02:38 -0500

    Get all computers, and details

--- BangazonWorkforceSapphireElephants/Controllers/ComputersController.cs
+++ BangazonWorkforceSapphireElephants/Controllers/ComputersController.cs
@@ -1,24 +1,108 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using BangazonWorkforceSapphireElephants.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace BangazonWorkforceSapphireElephants.Controllers
 {
     public class ComputersController : Controller
     {
+
+        /*
+         Establishing connection
+        */
+        private readonly IConfiguration _configuration;
+
+        public ComputersController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+            }
+        }
+        /*
+        Establishing connection
+       */
+
+
         // GET: Computers
         public ActionResult Index()
         {
-            return View();
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "select Id as cId, PurchaseDate as CPurchase, DecomissionDate as CDecom, Make as cMake, Manufacturer as CMan" +
+                        " from computer";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Computer> computerList = new List<Computer>();
+
+                    while (reader.Read())
+                    {
+                        Computer computer = new Computer
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("cId")),
+                            PurchaseDate = reader.GetDateTime(reader.GetOrdinal("CPurchase")),
+                            DecomissionDate = reader.GetDateTime(reader.GetOrdinal("CDecom")),
+                            Make = reader.GetString(reader.GetOrdinal("cMake")),
+                            Manufacturer = reader.GetString(reader.GetOrdinal("cMan"))
+                        };
+                        computerList.Add(computer);
+                    }
+
+                    reader.Close();
+                    return View(computerList);
+                }
+            }
         }
 
         // GET: Computers/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+
+                    cmd.CommandText = "select Id as cId, PurchaseDate as CPurchase, DecomissionDate as CDecom, Make as cMake, Manufacturer as CMan" +
+                        " from computer" +
+                        " WHERE computer.id =  @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+
+
+                    Computer computer = null;
+                    if (reader.Read())
+                    {
+                        computer = new Computer
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("cId")),
+                            PurchaseDate = reader.GetDateTime(reader.GetOrdinal("CPurchase")),
+                            DecomissionDate = reader.GetDateTime(reader.GetOrdinal("CDecom")),
+                            Make = reader.GetString(reader.GetOrdinal("cMake")),
+                            Manufacturer = reader.GetString(reader.GetOrdinal("cMan"))
+                        };
+
+                    }
+
+                    reader.Close();
+                    return View(computer);
+                }
+            }
         }
 
         // GET: Computers/Create
