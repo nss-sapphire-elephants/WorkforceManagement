3f7b508f4417b79aab42235620afbcee9a6ae4c2
Colleen Woolsey <rlwool@aol.com>
2019-04-11 14:10:22 -0500

    Department zero out

--- BangazonWorkforceSapphireElephants/Models/Department.cs
+++ BangazonWorkforceSapphireElephants/Models/Department.cs
@@ -1,15 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace BangazonWorkforceSapphireElephants.Models
-{
-    public class Department
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Budget { get; set; }
-        public List<Employee> Employees { get; set; } = new List<Employee>();
-    }
-}
+﻿
--- BangazonWorkforceSapphireElephants/Models/ViewModels/DepartmentCreateViewModel.cs
+++ BangazonWorkforceSapphireElephants/Models/ViewModels/DepartmentCreateViewModel.cs
@@ -1,11 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace BangazonWorkforceSapphireElephants.Models.ViewModels
-{
-    public class DepartmentCreateViewModel
-    {
-    }
-}
+﻿
