83b6438cba51e53f075404f56843ac3bd026c104
unknown <huntermettsdev@gmail.com>
2019-04-12 14:31:52 -0500

    Ticket 13 Complete

--- BangazonWorkforceSapphireElephants/Models/ViewModels/ComputerCreateViewModel.cs
+++ BangazonWorkforceSapphireElephants/Models/ViewModels/ComputerCreateViewModel.cs
@@ -3,9 +3,11 @@
  */
 
 
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -25,5 +27,56 @@ namespace BangazonWorkforceSapphireElephants.Models.ViewModels
 
         [Required]
         public DateTime? Assigned { get; set; }
+
+        [Required]
+        public int EmployeeId { get; set; }
+
+       
+            public ComputerCreateViewModel()
+            {
+                Employees = new List<Employee>();
+            }
+
+            public ComputerCreateViewModel(string connectionString)
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"SELECT Id, FirstName, LastName from Employee;";
+                        SqlDataReader reader = cmd.ExecuteReader();
+
+                        Employees = new List<Employee>();
+
+                        while (reader.Read())
+                        {
+                            Employees.Add(new Employee
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                LastName = reader.GetString(reader.GetOrdinal("LastName"))
+                            });
+                        }
+                        reader.Close();
+                    }
+                }
+            }
+
+        public List<Employee> Employees { get; set; }
+
+
+            public List<SelectListItem> EmployeeOptions
+            {
+                get
+                {
+                    return Employees.Select(c => new SelectListItem
+                    {
+                        Value = c.Id.ToString(),
+                        Text = c.FirstName
+                    }).ToList();
+                }
+            }
+
     }
 }
