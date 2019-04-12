95b5916ecaa48ac3e0362b8a44ae2a57d5077524
Nick Hansen <nick.hansen13@gmail.com>
2019-04-12 14:51:29 -0500

    Revert "get working for form"
    This reverts commit 3efe99778dd43a5ee63dba3d1bb0c9691aab2896.

--- BangazonWorkforceSapphireElephants/Controllers/EmployeesController.cs
+++ BangazonWorkforceSapphireElephants/Controllers/EmployeesController.cs
@@ -133,15 +133,14 @@ namespace BangazonWorkforceSapphireElephants.Controllers
                 return NotFound();
             }
 
-            EmployeeEditViewModel viewModel = new EmployeeEditViewModel
+            EmployeeEditViewModel viewModel = new EmployeeEditViewModel 
             //are these the things that need to be listed here? what are they = to?
             {
                 Employee = employee,
-                AddTrainingProgramList = GetAllAvailableTrainingPrograms(id),
-                EnrolledTrainingProgramsList = GetAllEnrolledTrainingPrograms(id),
-                ComputersList = GetAllAvailableComputers(),
+                AddTrainingProgramList =
+                EnrolledTrainingProgramsList =
+                ComputersList =
                 DepartmentsList = GetAllDepartments()
-                
                 
             };
 
@@ -267,7 +266,7 @@ namespace BangazonWorkforceSapphireElephants.Controllers
                 }
             }
         }
-        //-------------------------------------         MODULAR DROPDOWNS           -----------------------------------------
+        //-------------------------------------         MODULAR DEPARTMENT DROPDOWN            -----------------------------------------
         private List<Department> GetAllDepartments()
         {
             using (SqlConnection conn = Connection)
@@ -302,12 +301,8 @@ namespace BangazonWorkforceSapphireElephants.Controllers
                 conn.Open();
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
-                    //return computers which have an unassigned date, meaning, they are available to be reasigned
-                    cmd.CommandText = @"SELECT c.Id, c.Make
-                                            FROM Computer c
-                                            LEFT JOIN ComputerEmployee ce
-                                            ON c.Id = ce.ComputerId
-                                            WHERE ce.UnassignDate IS NOT NULL";
+                    cmd.CommandText = @"SELECT Id, Name 
+                                        FROM Department;";
                     SqlDataReader reader = cmd.ExecuteReader();
 
                     List<Computer> availableComputers = new List<Computer>();
@@ -317,86 +312,12 @@ namespace BangazonWorkforceSapphireElephants.Controllers
                         availableComputers.Add(new Computer
                         {
                             Id = reader.GetInt32(reader.GetOrdinal("Id")),
-                            Make = reader.GetString(reader.GetOrdinal("Make")),
-                                                                         
-                        });
-                    }
-                    reader.Close();
-
-                    return availableComputers;
-                }
-            }
-        }
-        private List<TrainingProgram> GetAllAvailableTrainingPrograms(int id)
-        {
-
-            
-            using (SqlConnection conn = Connection)
-            {
-                conn.Open();
-                using (SqlCommand cmd = conn.CreateCommand())
-                {
-                    //return computers which have an unassigned date, meaning, they are available to be reasigned
-                    cmd.CommandText = @"select tp.Id, tp.Name
-                                            FROM Employee e
-                                            LEFT JOIN EmployeeTraining et
-                                            ON e.Id = et.EmployeeId
-                                            LEFT JOIN TrainingProgram tp
-                                            ON et.TrainingProgramId = tp.Id
-                                            WHERE  e.Id != @id";
-                    cmd.Parameters.Add(new SqlParameter("@id", id));
-                    SqlDataReader reader = cmd.ExecuteReader();
-
-                    List<TrainingProgram> availableTrainingPrograms = new List<TrainingProgram>();
-
-                    while (reader.Read())
-                    {
-                        availableTrainingPrograms.Add(new TrainingProgram
-                        {
-                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
-                            Name = reader.GetString(reader.GetOrdinal("Name")),
-
-                        });
-                    }
-                    reader.Close();
-
-                    return availableTrainingPrograms;
-                }
-            }
-        }
-        private List<TrainingProgram> GetAllEnrolledTrainingPrograms(int id)
-        {
-
-
-            using (SqlConnection conn = Connection)
-            {
-                conn.Open();
-                using (SqlCommand cmd = conn.CreateCommand())
-                {
-                    //
-                    cmd.CommandText = @"select tp.Id, tp.Name
-                                            FROM Employee e
-                                            LEFT JOIN EmployeeTraining et
-                                            ON e.Id = et.EmployeeId
-                                            LEFT JOIN TrainingProgram tp
-                                            ON et.TrainingProgramId = tp.Id
-                                            WHERE  e.Id = @id";
-                    cmd.Parameters.Add(new SqlParameter("@id", id));
-                    SqlDataReader reader = cmd.ExecuteReader();
-
-                    List<TrainingProgram> enrolledTrainingPrograms = new List<TrainingProgram>();
-
-                    while (reader.Read())
-                    {
-                        enrolledTrainingPrograms.Add(new TrainingProgram
-                        {
-                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
                             Name = reader.GetString(reader.GetOrdinal("Name"))
                         });
                     }
                     reader.Close();
 
-                    return enrolledTrainingPrograms;
+                    return departments;
                 }
             }
         }
